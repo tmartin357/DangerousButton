f5bd76ac9274b6629f63090d88106a3498dd8a1e
tmartin357 <taylor.martin357@gmail.com>
2013-11-05 18:01:28 -0500

    Actually Added files that I previously linked

---
+++ DangerousButton/bin/Debug/dangerous.bat
@@ -0,0 +1,4 @@
+cd %3
+%2git pull https://github.com/spsu-auv-team/%1.git master
+echo. & echo. & echo. & echo If the number below is not Zero, somthing has gone wrong. & echo. & echo      %errorlevel%
+pause
